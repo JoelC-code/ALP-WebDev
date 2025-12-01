45052a1770f8ebd90a75ad39d6326b37da82bd0a
chris <christoperjoel1@gmail.com>
2025-12-01 17:44:44 +0700

    Deleting base migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Dom\Comment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -25,15 +26,21 @@ class User extends Authenticatable
         'password',
     ];
 
-    public function Boards(): HasMany {
-        return $this->hasMany(Board::class);
+    public function memberBoards(): HasMany {
+        return $this->hasMany(MemberBoard::class);
     }
 
-    public function Cards(): HasMany {
+    public function boards(): BelongsToMany {
+        return $this->belongsToMany(Board::class)
+                    ->using(MemberBoard::class)
+                    ->withPivot('role');
+    }
+
+    public function cards(): HasMany {
         return $this->hasMany(Card::class);
     }
 
-    public function Comments(): HasMany {
+    public function comments(): HasMany {
         return $this->hasMany(Comment::class);
     }
 }
